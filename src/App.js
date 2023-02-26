9dafbe75230cd2df7f1dffeedf13565db23d62ed
seregaxxl <seregaxxl@gmail.com>
2023-02-26 13:19:35 +0300

    learning to create hooks

--- src/App.js
+++ src/App.js
@@ -1,174 +1,120 @@
-import {Component, useState, useEffect, useCallback, useMemo} from 'react';
-import {Container} from 'react-bootstrap';
-import './App.css';
-// class Slider extends Component {
+// import {Component, useState, useEffect, useCallback, useMemo} from 'react';
+// import {Container} from 'react-bootstrap';
+// import './App.css';
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             autoplay: false,
-//             slide: 0
+// function logging() {
+//     console.log('log');
+// }
+
+// const countTotal = (num) => {
+//     console.log('counting');
+//     return num + 10;
+// }
+
+// const Slider = (props) => {
+
+//     const [slide, setSlide] = useState(0);
+//     const [autoplay, setAutoplay] = useState(false);
+
+//     const getSomeImages = useCallback(() => {
+//         console.log('fetching');
+//         return [
+//             "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+//             "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+//         ]
+//     },[])
+
+//     useEffect(() => {
+//         document.title = `Slide:${slide}`;
+//         window.addEventListener('click', logging);
+
+//         return () => {
+//             window.removeEventListener('click', logging);
 //         }
-//     }
+//     }, [slide]); 
 
-//     componentDidMount() {
-//         document.title = `Slide:${this.state.slide}`;
+//     function changeSlide(i) {
+//         setSlide(slide => slide + i)
+//     } 
+//     function toggleAutoplay() {
+//         setAutoplay(autoplay => !autoplay)
 //     }
+//     const total = useCallback(() => {
+//         return countTotal(slide)
+//     },[slide])
 
-//     componentDidUpdate() {
-//         document.title = `Slide:${this.state.slide}`;
-//     }
+//     const style = useMemo(() => 
+//         ({color: slide > 4 ? 'red' : 'black'})
+//     ,[slide])
 
-//     changeSlide = (i) => {
-//         this.setState(({slide}) => ({
-//             slide: slide + i
-//         }))
-//     }
+//     useEffect(()=>{
+//         console.log('style')
+//     },[style])
 
-//     toggleAutoplay = () => {
-//         this.setState(({autoplay}) => ({
-//             autoplay: !autoplay
-//         }))
-//     }
-
-//     render() {
-//         return (
-//             <Container>
-//                 <div className="slider w-50 m-auto">
-//                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                     <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                     <div className="buttons mt-3">
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(-1)}>-1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={() => this.changeSlide(1)}>+1</button>
-//                         <button 
-//                             className="btn btn-primary me-2"
-//                             onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                     </div>
+//     return (
+//         <Container>
+//             <div className="slider w-50 m-auto">
+//                 {
+//                     // getSomeImages().map((url,i)=>{
+//                     //     return <img key={i} className="d-block w-100" src={url} alt="slide" />
+ 
+//                     // })
+//                 }
+//                 <Slide getSomeImages={getSomeImages}/>
+                
+//                 <div className="text-center mt-5">Active slide {slide} <br/>
+//                  {autoplay ? 'auto' : null}
+//                  </div>
+//                  <div style={style} className="text-center mt-5">Total slides {total}
+//                  </div>
+//                 <div className="buttons mt-3">
+//                     <button 
+//                         className="btn btn-primary me-2"
+//                         onClick={() => changeSlide(-1)}>-1</button>
+//                     <button 
+//                         className="btn btn-primary me-2"
+//                         onClick={() => changeSlide(1)}>+1</button>
+//                     <button 
+//                         className="btn btn-primary me-2"
+//                         onClick={toggleAutoplay}>toggle autoplay</button>
 //                 </div>
-//             </Container>
-//         )
-//     }
+//             </div>
+//         </Container>
+//     )
+// }
+
+// const Slide = ({getSomeImages}) => {
+//     const [images, setImages] = useState([])
+//     useEffect(()=>{
+//         setImages(getSomeImages())
+//     },[getSomeImages])
+
+//     return (
+//         <>
+//         {images.map((url,i) => {
+//             return <img key={i} className="d-block w-100" src={url} alt="slide" />
+//         })}
+//         </>
+//     )
 // }
 
 
+// function App() {
+// const [slider, setSlider] = useState(true);
 
-function logging() {
-    console.log('log');
-}
+//   return (
+//         <>
+//         <button onClick={() => {
+//             setSlider(false)
+//         }}>
+//             Click
+//         </button>
+//         {slider ? <Slider/> : null}
+//         </>
+//   );
+// }
 
-const countTotal = (num) => {
-    console.log('counting');
-    return num + 10;
-}
-
-const Slider = (props) => {
-
-    const [slide, setSlide] = useState(0);
-    const [autoplay, setAutoplay] = useState(false);
-
-    const getSomeImages = useCallback(() => {
-        console.log('fetching');
-        return [
-            "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
-            "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
-        ]
-    },[])
-
-    useEffect(() => {
-        document.title = `Slide:${slide}`;
-        window.addEventListener('click', logging);
-
-        return () => {
-            window.removeEventListener('click', logging);
-        }
-    }, [slide]); 
-
-    function changeSlide(i) {
-        setSlide(slide => slide + i)
-    } 
-    function toggleAutoplay() {
-        setAutoplay(autoplay => !autoplay)
-    }
-    const total = useMemo(() => {
-        return countTotal(slide)
-    },[slide])
-
-    const style = useMemo(() => 
-        ({color: slide > 4 ? 'red' : 'black'})
-    ,[slide])
-
-    useEffect(()=>{
-        console.log('style')
-    },[style])
-
-    return (
-        <Container>
-            <div className="slider w-50 m-auto">
-                {
-                    // getSomeImages().map((url,i)=>{
-                    //     return <img key={i} className="d-block w-100" src={url} alt="slide" />
- 
-                    // })
-                }
-                <Slide getSomeImages={getSomeImages}/>
-                
-                <div className="text-center mt-5">Active slide {slide} <br/>
-                 {autoplay ? 'auto' : null}
-                 </div>
-                 <div style={style} className="text-center mt-5">Total slides {total}
-                 </div>
-                <div className="buttons mt-3">
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(-1)}>-1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={() => changeSlide(1)}>+1</button>
-                    <button 
-                        className="btn btn-primary me-2"
-                        onClick={toggleAutoplay}>toggle autoplay</button>
-                </div>
-            </div>
-        </Container>
-    )
-}
-
-const Slide = ({getSomeImages}) => {
-    const [images, setImages] = useState([])
-    useEffect(()=>{
-        setImages(getSomeImages())
-    },[getSomeImages])
-
-    return (
-        <>
-        {images.map((url,i) => {
-            return <img key={i} className="d-block w-100" src={url} alt="slide" />
-        })}
-        </>
-    )
-}
-
-
-function App() {
-const [slider, setSlider] = useState(true);
-
-  return (
-        <>
-        <button onClick={() => {
-            setSlider(false)
-        }}>
-            Click
-        </button>
-        {slider ? <Slider/> : null}
-        </>
-  );
-}
-
-export default App;
+// export default App;
 
 
 
@@ -228,3 +174,62 @@ export default App;
   // 2) INC и DEC увеличивают и уменьшают счетчик соответственно на 1. Без ограничений, но можете добавить границу в -50/50. По достижению границы ничего не происходит
   // 3) RND изменяет счетчик в случайное значение от -50 до 50. Конструкцию можете прогуглить за 20 секунд :) Не зависит от предыдущего состояния
   // 4) RESET сбрасывает счетчик в 0 или в начальное значение из пропсов. Выберите один из вариантов
+
+import { useState, useEffect} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+function useInputsWithValidate(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const validateInput = () => {
+        return value.search(/\d/) >= 0
+    }
+
+    return {value, onChange, validateInput}
+}
+
+const Form = () => {
+
+    const text = useInputsWithValidate('');
+    const textArea = useInputsWithValidate('');
+
+
+    const color = text.validateInput(text) ? 'text-danger' : null
+
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <input value={`${text.value} / ${textArea.value}`} type="text" className="form-control" readOnly/>
+                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
+                    <input 
+                    onChange={text.onChange} 
+                    type="email" 
+                    value={text.value}
+                    className={`form-control ${color}`} 
+                    id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea
+                    onChange={textArea.onChange}
+                    value={textArea.value}
+                    className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
+    )
+}
+
+function App() {
+    return (
+        <Form/>
+    );
+}
+
+export default App;
